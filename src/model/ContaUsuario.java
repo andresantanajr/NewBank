f8ba23853ae9399eaf300cf2f85f02f7180156f4
andresantanajr <andre21mugt@gmail.com>
2021-03-28 21:46:21 -0300

    Atualizacao final

--- src/model/ContaUsuario.java
+++ src/model/ContaUsuario.java
@@ -5,7 +5,7 @@ public class ContaUsuario {
 		
 		ContaCorrente cc1 = new ContaCorrente();
 		ContaPoupanca cp1 = new ContaPoupanca();
-		ContaSalario cs1 = new ContaSalario();	
+		ContaSalario cs1 = new ContaSalario();
 		
 		
 	}
